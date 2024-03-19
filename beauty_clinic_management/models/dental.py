931b0c2ff91613d75cc3d68e7237882393df164a
vanillabush <iykemarkhenry@gmail.com>
2024-03-19 22:03:49 +0100

    [add] add ap_type on event view 5

--- beauty_clinic_management/models/dental.py
+++ beauty_clinic_management/models/dental.py
@@ -1521,13 +1521,6 @@ class MedicalAppointment(models.Model):
         else:
             return False
 
-    # def get_data(self, doctor_id, event_name, index, appointment_type):
-    #     record = self.search([('name', '=', event_name), ('doctor', '=', doctor_id), ('appointment_type', '=',appointment_type)], limit=1)
-    #     if record:
-    #         return {'index': index, 'patient': record.patient.partner_id.name, "services": ",".join(record.services_ids.mapped("name")), "appointment_type": record.appointment_type}
-    #     else:
-    #         return {'index': 0, 'patient': record.patient.partner_id.name, "services": ",".join(record.services_ids.mapped("name")), "appointment_type": record.appointment_type}
-
     def get_data(self, doctor_id, event_name, index):
         record = self.search([('name', '=', event_name), ('doctor', '=', doctor_id)], limit=1)
         if record:
